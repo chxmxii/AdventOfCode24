a70eeb54b8513c7d0d7aabc9d5b204a4f46642d1
Mouhib Chamsi <94466413+chxmxii@users.noreply.github.com>
2024-12-09 15:19:01 +0000

    added main.go

--- Day 1/main.go
+++ Day 1/main.go
@@ -15,9 +15,23 @@ func check(e error) {
 	}
 }
 
+func Abs(n int) int {
+    if n >= 0 {
+        return n
+    }
+    return -n
+}
+
 func main() {
-	left, right := utils.ReadFile(*filename)
+	left, right := utils.ReadFile(filename)
+	// left := []int{3,4,2,1,3,3}
+	// right := []int{4,3,5,3,9,3}
 	sort.Ints(left)
 	sort.Ints(right)
-
+	sum := 0
+	for i := 0; i < len(left); i++ {
+		distance := left[i] - right[i]
+		sum = sum + Abs(distance)
+	}
+	fmt.Println(sum)
 }
