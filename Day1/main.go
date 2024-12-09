26993e36b097000722ac9d5304614053e437b290
Mouhib Chamsi <94466413+chxmxii@users.noreply.github.com>
2024-12-09 16:10:33 +0000

    added the day 1 part 2

--- Day1/main.go
+++ Day1/main.go
@@ -23,6 +23,16 @@ func Abs(n int) int {
     return -n
 }
 
+func Frequency(right []int, num int) int {
+	c := 0
+	for _, item := range right {
+		if item == num {
+			c++
+		}
+	} 
+	return c
+}
+
 func main() {
 	flag.Parse()
 	left, right := utils.ReadFile(*filename)
@@ -32,8 +42,13 @@ func main() {
 	sort.Ints(right)
 	sum := 0
 	for i := 0; i < len(left); i++ {
-		distance := left[i] - right[i]
-		sum = sum + Abs(distance)
+		sum += Abs(left[i] - right[i])
 	}
-	fmt.Println(sum)
+	fmt.Println("Distance is:",sum)
+	// part II
+	ss := 0
+	for i := 0; i < len(left); i++ {
+		ss += left[i] * Frequency(right,left[i])
+	}
+	fmt.Println("Similarty Score is",ss)
 }
