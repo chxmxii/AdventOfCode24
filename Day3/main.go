410e8d7c8a34c597f6d1938d2a2f9c1cecedc4ad
Mouhib Chamsi <94466413+chxmxii@users.noreply.github.com>
2024-12-11 16:25:08 +0000

    finished day 3 part 1

--- Day3/main.go
+++ Day3/main.go
@@ -1,61 +1,48 @@
 package main
 
+
 import (
-	"fmt"
-	"strings"
 	"regexp"
+	"strings"
 	"strconv"
-	_"reflect"
+	_ "reflect"
+	"fmt"
+	_ "embed"
 )
 
-var (
-	input = "};//how():mul(422,702)'how()'from()-&when(551,888)from()#mul(694,437)who()<,/ ~)"
-	ptrn1 = `mul\([\d]{1,3},[\d]{1,3}\)` //first pattern used to fetch the safe funcs
-	ptrn2 = `[\d]{1,3},[\d]{1,3}` // second pattern used to fetch the nums
-)
+//go:embed input.txt
+var input string
 
 func main() {
-	data :=ClearString(input)
-	fmt.Println("this is main", IntArray(data))
-	// nums := []int
-	// sum := 0
-	// data := regexp.MustCompile(pattern).FindAllString(test,-1)
-	// nums := regexp.MustComplie(pattern2).FindAllString(data,-1)
-	// fmt.Println(data)
-	// for _,n := range data {
-	// 	nums := regexp.MustCompile(pattern2).FindAllString(n,-1)
-	// 	fmt.Println(nums)
-	// 	// fmt.Println(reflect.TypeOf(nums))
-	// }
-}
-
-func ClearString(input string) []string {
-	nums := make([]string, 0)
-	data := regexp.MustCompile(ptrn1).FindAllString(input,-1)
-	fmt.Println("this cs ",data)
-	for i := range data {
-		fmt.Println("this cs ddd",data[i])
-		nums := regexp.MustCompile(ptrn2).FindAllString(i,-1)
-		fmt.Println("this is cs too", nums)
-		return nums
+	input := strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input!!!")
 	}
-	return nums
+	sum := 0
+	data := clearString(input)
+	for _, d := range data {
+		sum += getNums(d)
+	}
+	fmt.Println(sum)
 }
 
-func IntArray(input []string) []int {
-	// nums := ClearString(input)
-	array := make([]int, 0)
-	fmt.Println("this is intarr",input)
-	for _, n := range input {
-		fmt.Println("this intarr",strings.Split(n,","))
-		str := strings.Split(n,",")
-		for _, s := range str {
-			value, _ := strconv.Atoi(s)
-			fmt.Println("this intarr vv",value)
-			array = append(array, value)
+func clearString(input string) []string {
+	data := regexp.MustCompile(`mul\([\d]{1,3},[\d]{1,3}\)`).FindAllString(input,-1)
+	return data 
+}
+
+func getNums(input string) int {
+	nums := make([]int, 0)
+	values := regexp.MustCompile(`\d+`).FindAllString(input,-1)
+	for _, v := range values {
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Println("Cannot convert string to int:", err)
+			continue
 		}
-	}
-	return array
+		nums = append(nums,num)
+	} 
+	return mul(nums[0],nums[1])
 }
 
 func mul(x,y int) int {
